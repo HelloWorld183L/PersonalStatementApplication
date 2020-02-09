cb7f0fef0ed40af36bf1ee7fa7bbe968923c3a2f
HelloWorld183L <46497907+HelloWorld183L@users.noreply.github.com>
2020-02-09 22:21:33 +0000

    Added converter
    I don't use the converter currently.

---
+++ Converters/QuestionsTextConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PersonalStatementTool.Presentation2.Converters
+{
+    public class QuestionsTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.WriteLine("Value = " + value);
+            Debug.WriteLine("Parameter = " + parameter);
+            return new object();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.WriteLine(value);
+            Debug.WriteLine(parameter);
+            return new object();
+        }
+    }
+}
