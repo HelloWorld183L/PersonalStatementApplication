a2a49458f0b01bb8b47eec2ec89bf2b6bf0b072a
HelloWorld183L <46497907+HelloWorld183L@users.noreply.github.com>
2020-02-09 23:10:47 +0000

    Add files via upload

---
+++ PersonalStatementTool.Core/Services/PersonalStatementService.cs
@@ -0,0 +1,41 @@
+﻿namespace PersonalStatementTool.Core
+{
+    public class PersonalStatementService : IPersonalStatementService
+    {
+        public PersonalStatement PersonalStatement { get; set; }
+
+        public PersonalStatementService()
+        {
+            PersonalStatement = new PersonalStatement();
+        }
+
+        public void SetNewText(string newText)
+        {
+            var personalStatementContent = PersonalStatement.Content;
+
+            if (personalStatementContent.Contains(newText))
+            {
+                personalStatementContent.Remove(newText);
+            }
+            personalStatementContent.Add(newText);
+
+            if (personalStatementContent.Contains(string.Empty))
+            {
+                RemoveEmptyElements();
+            }
+
+            PersonalStatement.Text = string.Join("/n", personalStatementContent);
+        }
+
+        private void RemoveEmptyElements()
+        {
+            foreach (var element in PersonalStatement.Content)
+            {
+                if (element == string.Empty)
+                {
+                    PersonalStatement.Content.Remove(element);
+                }
+            }
+        }
+    }
+}
