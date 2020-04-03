769e68449c02f9f75d3091d8be2c722156315eb7
HelloWorld183L <46497907+HelloWorld183L@users.noreply.github.com>
2020-04-03 15:34:04 +0100

    Add files via upload

---
+++ PersonalStatementTool.Core/Common/PersonalStatementMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonalStatementTool.Core
+{
+    public class PersonalStatementMessage
+    {
+        public string Text { get; set; }
+        private IEnumerable<string> _content { get; set; }
+        public MessageSource MessageSource { get; set; }
+
+        public PersonalStatementMessage() { }
+
+        public PersonalStatementMessage(MessageSource messageSource, IEnumerable<string> content)
+        {
+            MessageSource = messageSource;
+            _content = content;
+
+            if (content != null && _content.Count() > 0) Text = string.Join("\n", _content);
+        }
+    }
+}
